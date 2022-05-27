26828f7a19e77e55096513e6bbb057a57194f627
Vitor Müller <85710199+vitox013@users.noreply.github.com>
2022-05-27 01:44:43 -0300

    agora cada posicao do vetor historia é uma palavra

--- trabalho-3/main.c
+++ trabalho-3/main.c
@@ -1,36 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+int tam = 0;
+typedef struct {
+    char palavra[20];
+    char sinais[1];
+}P;
 
-
-void ler_arquivo(char historia[]){
+void ler_arquivo(P historia[]){
     FILE* file = fopen("historia.txt", "r");
-    int i = 0;
+    char vet[50];
+    
+    char tmp;
     if(file){
-        for(i; (historia[i] = fgetc(file)) != EOF; i++){ 
+        while (!feof(file) && !ferror(file)){
+            tmp = fgetc(file);
+            int i = 0;
+            while ((tmp != ' ') ){
+                vet[i] = tmp;
+                tmp = fgetc(file);
+                if (tmp == EOF){
+                    break;
+                }
+                i++;
+            }
+            vet[i] = '\0';
+            if(vet[0] != '\0'){
+                strcpy(historia[tam].palavra,vet);
+                tam++;
+            }
         }
-        historia[i] = '\0';
+        printf("\nArquivo lido com sucesso\n");
     }
     else
         printf("Erro ao abrir o arquivo");
     fclose(file);
 }
 
+void arquivo_invertido(P historia[]){
+    int i = tam;
+    while (i != 0)
+    {
+        printf("%s ", historia[i].palavra);
+        i--;
+    }
+    printf("\n\n");
+}
+
 void menu_opcoes(){
-    printf("\na - Ler um arquivo texto\nb - Apresentar o arquivo invertido\nc - Procurar uma palavra.\n");
+    printf("\na - Ler um arquivo texto\nb - Apresentar o arquivo invertido\nc - Procurar uma palavra.\n\n");
 }
 
 int main(){
 
-    char historia[5876];
+    P historia[1000];
     char opcao;
 
-    menu_opcoes();
-    scanf("%c", &opcao);
-    switch(opcao){
-
-    case 'a':
-        ler_arquivo(historia);
-    }
-
+    do
+    {
+        menu_opcoes();
+        scanf("%c", &opcao);
+        getchar();
+        switch(opcao){
+        case 'a':
+            system("clear");
+            ler_arquivo(historia);
+            break;
+        case 'b':
+            system("clear");
+            arquivo_invertido(historia);
+            break;
+        }
+    }while (opcao != 'e');
+    
     return 0;
 }
